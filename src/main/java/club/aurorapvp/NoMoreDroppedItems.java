468e2aac0d251e3bf72158a68518f713dcf088b1
Villagers654 <thbvill@gmail.com>
2022-11-23 18:52:42 +0900

    Remove unneccesary stuff

--- src/main/java/club/aurorapvp/NoMoreDroppedItems.java
+++ src/main/java/club/aurorapvp/NoMoreDroppedItems.java
@@ -5,16 +5,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NoMoreDroppedItems extends JavaPlugin implements Listener {
-
   @Override
   public void onEnable() {
-    // Plugin startup logic
     getServer().getPluginManager().registerEvents(new EventListener(), this);
-
-  }
-
-  @Override
-  public void onDisable() {
-    // Plugin shutdown logic
   }
 }
