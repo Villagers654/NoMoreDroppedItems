1db5e18fd050907d4ef24965953c50b25a28e536
Villagers654 <thbvill@gmail.com>
2023-06-30 21:14:22 +0900

    Last time I touch this

--- src/main/java/club/aurorapvp/listeners/EventListener.java
+++ src/main/java/club/aurorapvp/listeners/EventListener.java
@@ -2,26 +2,19 @@ package club.aurorapvp.listeners;
 
 import club.aurorapvp.config.Config;
 import java.util.Objects;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class EventListener implements Listener {
   @EventHandler
   public void onItemSpawn(PlayerDropItemEvent event) {
-    if (event.getItemDrop().getType() == EntityType.DROPPED_ITEM) {
-      for (Object entry : Objects.requireNonNull(Config.get().getList("excluded-items"))) {
-        ItemStack item = Bukkit.getItemFactory().createItemStack((String) entry);
-
-        if (event.getItemDrop().getItemStack().getType() == item.getType()) {
-          return;
-        }
+    for (Object entry : Objects.requireNonNull(Config.get().getList("excluded-items"))) {
+      if (event.getItemDrop().getItemStack().getType().name().equals((String) entry)) {
+        return;
       }
-      event.setCancelled(true);
     }
+
+    event.setCancelled(true);
   }
 }
