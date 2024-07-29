3bcc5f6c8e8e01c4b04ed0528bc9ca0d2c7604df
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2024-07-29 17:42:16 -0400

    Prevent item drops on explosions

--- src/main/java/club/aurorapvp/nomoredroppeditems/listeners/EventListener.java
+++ src/main/java/club/aurorapvp/nomoredroppeditems/listeners/EventListener.java
@@ -1,5 +1,6 @@
 package club.aurorapvp.nomoredroppeditems.listeners;
 
+import club.aurorapvp.nomoredroppeditems.NoMoreDroppedItems;
 import club.aurorapvp.nomoredroppeditems.config.Config;
 import club.aurorapvp.nomoredroppeditems.flags.Flags;
 import com.destroystokyo.paper.event.block.BlockDestroyEvent;
@@ -12,18 +13,26 @@ import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
 import com.sk89q.worldguard.protection.regions.RegionQuery;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class EventListener implements Listener {
+
+  private static final Set<ItemStack> ITEMS_TO_REMOVE = ConcurrentHashMap.newKeySet();
+
   @EventHandler
   public void onBlockBroken(BlockBreakEvent event) {
     Block block = event.getBlock();
@@ -39,6 +48,30 @@ public class EventListener implements Listener {
 
     if (shouldPreventDrop(Flags.BLOCK_DROPS_ENABLED, block.getDrops(), block.getLocation())) {
       event.setWillDrop(false);
+    }
+  }
+
+  @EventHandler(priority = EventPriority.MONITOR)
+  public void onBlockExplode(BlockExplodeEvent event) {
+    for (Block block : event.blockList()) {
+      ITEMS_TO_REMOVE.addAll(block.getDrops());
+
+      new BukkitRunnable() {
+        @Override
+        public void run() {
+          ITEMS_TO_REMOVE.removeAll(block.getDrops());
+        }
+      }.runTaskLaterAsynchronously(NoMoreDroppedItems.INSTANCE, 1);
+    }
+  }
+
+  @EventHandler(priority = EventPriority.LOWEST)
+  public void onItemSpawn(ItemSpawnEvent event) {
+    ItemStack items = event.getEntity().getItemStack();
+
+    if (ITEMS_TO_REMOVE.contains(items)
+        && shouldPreventDrop(Flags.BLOCK_DROPS_ENABLED, Set.of(items), event.getLocation())) {
+      event.getEntity().remove();
     }
   }
 
